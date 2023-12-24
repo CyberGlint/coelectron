be6fa547ae4f8fd39cc629a4e0f68fc8608b1223
SUE <496966859@qq.com>
2023-12-24 12:00:21 +0800

    优化分布式锁

--- coelectron-redis/src/main/java/com/cyberglint/redis/util/CyberGlintRedisUtil.java
+++ coelectron-redis/src/main/java/com/cyberglint/redis/util/CyberGlintRedisUtil.java
@@ -1,5 +1,6 @@
 package com.cyberglint.redis.util;
 
+import com.cyberglint.redis.enums.CyberGlintLockStrategyEnum;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
@@ -20,8 +21,7 @@ public class CyberGlintRedisUtil {
     
     @Resource
     private RedisTemplate<String, Object> redisTemplate;
-    //
-    @Autowired
+    @Resource
     private RedissonClient redissonClient;
     
     /**
@@ -80,36 +80,39 @@ public class CyberGlintRedisUtil {
     /**
      * 使用分布式锁执行一个操作。
      *
-     * @param lockKey     锁的键。
-     * @param action      要执行的操作，使用 Lambda 表达式。
-     * @param lockTimeout 获取锁的最大等待时间（秒）。
-     * @param leaseTime   锁的持有时间（秒），之后锁会自动释放。
+     * @param lockKey 锁的键。
+     * @param action  要执行的操作，使用 Lambda 表达式。
      */
-    public void executeWithLock(String lockKey, Runnable action, long lockTimeout, long leaseTime) {
+    public void executeWithLock(
+            String lockKey, Runnable action,
+            CyberGlintLockStrategyEnum cyberGlintLockStrategyEnum
+    ) {
         RLock lock = redissonClient.getLock(lockKey);
         boolean isLocked = false;
         
         try {
-            log.info("尝试获取锁-{}", lockKey);
-            isLocked = lock.tryLock(lockTimeout, leaseTime, TimeUnit.SECONDS);
+            log.info("尝试获取锁 {}", lockKey);
+            isLocked = lock.tryLock(cyberGlintLockStrategyEnum.getLockTimeout(),
+                    cyberGlintLockStrategyEnum.getLeaseTime(), TimeUnit.SECONDS
+            );
             
             if (isLocked) {
-                log.info("成功获取锁-{}", lockKey);
+                log.info("成功获取锁 {}", lockKey);
                 action.run();
-                log.info("操作成功执行-{}", lockKey);
+                log.info("操作成功执行 {}", lockKey);
             } else {
-                log.warn("无法获取锁-{}", lockKey);
+                log.warn("无法获取锁 {}", lockKey);
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
-            log.error("获取锁时被中断-{}", lockKey, e);
+            log.error("获取锁时被中断 {}", lockKey, e);
         } catch (Exception e) {
-            log.error("执行操作时出现异常-{}", lockKey, e);
+            log.error("执行操作时出现异常 {}", lockKey, e);
             throw e;
         } finally {
-            if (isLocked) {
+            if (isLocked && lock.isHeldByCurrentThread()) {
                 lock.unlock();
-                log.info("释放锁-{}", lockKey);
+                log.info("释放锁 {}", lockKey);
             }
         }
     }
