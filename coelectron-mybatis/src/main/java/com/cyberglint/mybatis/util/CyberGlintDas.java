26612ffe9b2e87a8b1bf6ed011ace4959df9f3ee
SUE <496966859@qq.com>
2024-01-02 21:23:02 +0800

    新增数据操作层

--- coelectron-mybatis/src/main/java/com/cyberglint/mybatis/util/CyberGlintDas.java
+++ coelectron-mybatis/src/main/java/com/cyberglint/mybatis/util/CyberGlintDas.java
@@ -8,7 +8,6 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
  * @description
  * @date 2021/9/18
  */
-
 public class CyberGlintDas<M extends BaseMapper<T>, T> extends ServiceImpl<M, T> {
 
 }
