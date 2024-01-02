0ca1cf21506851f6d7020a82bab634fb3bb16143
SUE <496966859@qq.com>
2024-01-02 20:36:35 +0800

    新增公共Eo

---
+++ coelectron-common/src/main/java/com/cyberglint/common/CyberGlintBaseEo.java
@@ -0,0 +1,29 @@
+package com.cyberglint.common;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class CyberGlintBaseEo {
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+    /**
+     * 创建人
+     */
+    private String createPerson;
+    /**
+     * 更新人
+     */
+    private String updatePerson;
+    /**
+     * 逻辑删除 0:未删除 1:删除
+     */
+    private Integer dr;
+}
