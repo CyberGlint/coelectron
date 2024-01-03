c8115eca3f08a3ec3ea9a5232ed7917bf168f7ef
LPPZ-IT-SHUSHUN-20046382 <496966859@qq.com>
2024-01-03 18:02:39 +0800

    完善结构

--- coelectron-mybatis/src/main/java/com/cyberglint/mybatis/util/CyberGlintDas.java
+++ coelectron-mybatis/src/main/java/com/cyberglint/mybatis/util/CyberGlintDas.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.cyberglint.common.CyberGlintBaseEo;
+import org.springframework.beans.BeanUtils;
 
 /**
  * 基础数据层
@@ -17,4 +18,23 @@ public class CyberGlintDas<M extends BaseMapper<T>, T extends CyberGlintBaseEo> 
         queryWrapper.select(T::getId);
         return super.getOne(queryWrapper) != null;
     }
+    
+    
+    /**
+     * 将查询出的数据转换为给定的 VO 类型
+     */
+    public <V> V convertEntityToVo(T entity, Class<V> voClass) {
+        if (entity == null) {
+            return null;
+        }
+        V vo = null;
+        try {
+            vo = voClass.newInstance();
+            BeanUtils.copyProperties(entity, vo);
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace(); // 建议使用更合适的错误处理逻辑
+        }
+        return vo;
+    }
+    
 }
